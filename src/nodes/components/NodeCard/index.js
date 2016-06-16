081d1da303ce93acc533c097261510f6b07a7f2c
denisfl <delchyve@gmail.com>
2016-06-16 11:57:12 +0300

    refactor NodeCard

--- src/nodes/components/NodeCard/index.js
+++ src/nodes/components/NodeCard/index.js
@@ -1,14 +1,32 @@
 import React from "react";
 import {Link} from "react-router";
-import {Card,CardHeader} from "material-ui"
-import moment from 'moment'
+import {Avatar} from "material-ui";
+import {colors} from "material-ui/styles";
+
+import {Card, CardItem, CardDivider, CardRows, CardRow} from "commons/cards/components";
+import moment from 'moment';
 
 export default ({node}) => {
-  return (node && <Card>
-    <CardHeader
-        title={<Link to={"/nodes/"+node.id} style={{color:'#333'}}>{ node.title }</Link>}
-        subtitle={<span>{node.user.name}{ node.firstPublished && <span>, <i>{moment(node.firstPublished).fromNow()}</i></span> } </span>}
-        avatar={node.user.imageId ? ("/api/images/"+node.user.imageId + "?q=80&w=80&h=80&m=cover") : node.user.avatarUrl}
-    />
-  </Card>)
+  return (node &&
+    <Card>
+      <CardItem>
+        <Avatar
+          color={ colors.deepOrange300 }
+          backgroundColor={ colors.purple500 }
+          src={node.user.imageId ? ("/api/images/"+node.user.imageId + "?q=80&w=80&h=80&m=cover") : node.user.avatarUrl}
+        />
+      </CardItem>
+      <CardItem>
+        <CardRows>
+          <Link to={"/nodes/"+node.id} style={{color:'#333'}}>{ node.title }</Link>
+        </CardRows>
+        <CardRows>
+          <CardRow>{node.user.name}</CardRow>
+          <CardRow>
+            { node.firstPublished && <span>, <i>{moment(node.firstPublished).fromNow()}</i></span> }
+          </CardRow>
+        </CardRows>
+      </CardItem>
+    </Card>
+  )
 }
