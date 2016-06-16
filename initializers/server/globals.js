4412d864db901be1c3458993253ee054bcdc927b
Dmitry Kurinskiy <name.alari@gmail.com>
2016-06-16 10:56:46 +0300

    better globals.js

--- initializers/server/globals.js
+++ initializers/server/globals.js
@@ -4,6 +4,6 @@ global.__CLIENT__ = false;
 global.__SERVER__ = true;
 global.__DEVTOOLS__ = false;
 global.__ENV__ = config.env;
-global.__REQUEST_API_ENDPOINT__ = config.api.request ? config.api.request.endpoint : `http://mirari.ru/api`;
+global.__REQUEST_API_ENDPOINT__ = (config.api.remote ? (config.api.remote.host + config.api.remote.path) : `http://mirari.ru/`) + config.api.root;
 global.__AUTH_LOGIN_HREF__ = `/auth/in`;
 global.__AUTH_FORBIDDEN_HREF__ = `/`;
