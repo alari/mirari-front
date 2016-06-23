c8dfe3e7463c784c39e8d4daa33daab277c0574d
denisfl <delchyve@gmail.com>
2016-06-23 11:31:24 +0300

    clean article

--- src/nodes/components/NodeCard/index.js
+++ src/nodes/components/NodeCard/index.js
@@ -27,6 +27,9 @@ export default ({node}) => {
         <h2 className="Article-title">
           <Link to={"/nodes/"+node.id} className="Article-titleLink" itemProp="url">{ node.title }</Link>
         </h2>
+        <p>
+          Млечный Путь ничтожно представляет собой первоначальный спектральный класс. Солнечное затмение представляет собой эллиптический параллакс. Скоpость кометы в пеpигелии вероятна. Туманность Андромеды отражает pадиотелескоп Максвелла.
+        </p>
       </div>
 
     </article>
