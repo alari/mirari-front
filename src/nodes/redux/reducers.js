f402a6e9762361d892d832769af6c07071202f1e
Dmitry Kurinskiy <name.alari@gmail.com>
2016-06-27 12:32:50 +0300

    with sse

--- src/nodes/redux/reducers.js
+++ src/nodes/redux/reducers.js
@@ -1,12 +1,6 @@
 import {concat, map, filter, groupBy, find} from "ramda";
 import {createReducer, update} from "commons/utils";
-import {
-  NODES_LIST,
-  NODES_GET,
-  NODES_SAVE,
-  NODES_DELETE,
-  NODES_COMMENT
-} from "./constants";
+import {NODES_LIST, NODES_GET, NODES_SAVE, NODES_DELETE, NODES_COMMENT, NODE_SSE_COMMENT} from "./constants";
 
 
 const updateNodeInStore = (state, id, handler) => {
@@ -42,27 +36,27 @@ const updateNodeInStore = (state, id, handler) => {
 
 const prepareComments = (comments) => {
   const byReplyId = groupBy(c => c.replyTo || "node", comments.values)
-  map(c => c.children = byReplyId[c.id] || [] , comments.values)
+  map(c => c.children = byReplyId[c.id] || [], comments.values)
   return byReplyId.node
 }
 
 const addComment = (comments, add) => {
   add.children = []
-  if(!comments) comments = []
-  if(!add.replyTo) {
-    comments.push(add)
+  if (!comments) comments = []
+  if (!add.replyTo) {
+    find(h => h.id === add.id, comments) || comments.push(add)
     return comments
   } else {
     let i = 0
-    while(i < comments.length) {
+    while (i < comments.length) {
       const c = comments[i]
-      if(c.id === add.replyTo) {
-        find(h => h.id === add.id) || c.children.push(add)
+      if (c.id === add.replyTo) {
+        find(h => h.id === add.id, c.children) || c.children.push(add)
         break
       } else {
         addComment(c.children, add)
       }
-      i+=1
+      i += 1
     }
     return comments
   }
@@ -71,19 +65,22 @@ const addComment = (comments, add) => {
 export default createReducer({}, {
   [NODES_LIST.SUCCESS]: (state, action) => ({
     ...state,
-    list: action.append ? {...action.result.body, values: concat(state.list.values || [], action.result.body.values)} : action.result.body
+    list: action.append ? {
+      ...action.result.body,
+      values: concat(state.list.values || [], action.result.body.values)
+    } : action.result.body
   }),
 
   [NODES_GET.REQUEST]: (state, action) => ({
-      ...state,
-      node: null
-    }),
+    ...state,
+    node: null
+  }),
 
   [NODES_GET.SUCCESS]: (state, action) => ({
-      ...state,
-      node: action.result.body,
+    ...state,
+    node: action.result.body,
     comments: action.result.body.comments && prepareComments(action.result.body.comments)
-    }),
+  }),
 
   [NODES_SAVE.REQUEST]: (state, action) =>
     updateNodeInStore(state, action.nodeId, (node) => update.set(node, action.params)),
@@ -92,7 +89,7 @@ export default createReducer({}, {
     const updated = updateNodeInStore(state, action.nodeId, (node) => {
       return update.commit(node, action.result.body)
     })
-    if(action.result.status === 201 && updated.list && updated.list.values) {
+    if (action.result.status === 201 && updated.list && updated.list.values) {
       updated.list.values.unshift(action.result.body)
     }
     return updated
@@ -103,14 +100,26 @@ export default createReducer({}, {
 
   [NODES_COMMENT.SUCCESS]: (state, action) => ({
     ...state,
-     node: {
-       ...state.node,
-       comments: {
-         total: state.node.comments.total + 1,
-         values: concat(state.node.comments.values, action.result.body)
-       }
-     },
+    node: {
+      ...state.node,
+      comments: {
+        total: state.node.comments.total + 1,
+        values: concat(state.node.comments.values, action.result.body)
+      }
+    },
     comments: addComment(state.comments, action.result.body)
+  }),
+
+  [NODE_SSE_COMMENT]: (state, action) => ({
+    ...state,
+    node: {
+      ...state.node,
+      comments: state.node.id !== action.nodeId ? state.node.comments : {
+        total: state.node.comments.total + 1,
+        values: concat(state.node.comments.values, action.data)
+      }
+    },
+    comments: addComment(state.comments, action.data)
   }),
 
   [NODES_DELETE.SUCCESS]: (state, action) => ({
--- src/store/sagas.js
+++ src/store/sagas.js
@@ -1,9 +1,13 @@
 import cookieSaga from "commons/auth/sagas/cookie"
 import resolveStateSaga from "commons/resolve/sagas/resolve";
 import nodeCreatedSaga from "nodes/sagas/nodeCreated";
+import nodeEnterSaga from "nodes/sagas/nodeEnter";
+import nodeSseSaga from "nodes/sagas/nodeSse";
 
 export default  [
   cookieSaga,
   resolveStateSaga,
+  nodeSseSaga,
+  nodeEnterSaga,
   nodeCreatedSaga
 ]
