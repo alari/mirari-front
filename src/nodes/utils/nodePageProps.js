6675e820ef5b7523ad1969f882dec19783be6d4d
Dmitry Kurinskiy <name.alari@gmail.com>
2016-07-15 23:52:43 +0300

    FQURL for og:image

--- src/nodes/utils/nodePageProps.js
+++ src/nodes/utils/nodePageProps.js
@@ -1,10 +1,10 @@
 export default (n) => ({
   title: n.title,
     meta: [
-    {property:"og:url", content:"/nodes/"+n.id},
+    {property:"og:url", content:"https://mirari.ru/nodes/"+n.id},
     {property:"og:type", content:"article"},
     {property:"og:title", content:n.title},
-    {property:"og:image", content:n.user.imageId ? ("/api/images/"+n.user.imageId+"?q=80") : n.user.avatarUrl},
+    {property:"og:image", content:n.user.imageId ? ("https://mirari.ru/api/images/"+n.user.imageId+"?q=80") : n.user.avatarUrl},
     {property:"og:description", content: (n.description || ((n.text && n.text.content) ? n.text.content.substring(0,300) : n.title))},
   ]
 })
