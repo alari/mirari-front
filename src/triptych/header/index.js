e6b57efccfb67c51ada8b0b45fd3c074eab0d23e
denisfl <delchyve@gmail.com>
2016-07-19 16:14:25 +0300

    update header

--- src/triptych/header/index.js
+++ src/triptych/header/index.js
@@ -2,7 +2,7 @@ import './styles.css';
 import React from 'react';
 import classnames from 'classnames';
 
-const AppHeader = ({ button, children, isCenter, title }) => {
+const AppHeader = ({ button, isCenter, title }) => {
   const rootClasses = classnames(
     'AppHeader-top-title',
     {
@@ -15,9 +15,6 @@ const AppHeader = ({ button, children, isCenter, title }) => {
       <div className="AppHeader-container AppHeader-top">
         <h2 className={rootClasses}>{title}</h2>
         <div className="AppHeader-rightControl">{button}</div>
-      </div>
-      <div className="AppHeader-container AppHeader-container--gray">
-        {children}
       </div>
     </div>
   );
