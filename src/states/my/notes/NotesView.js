59e322d2cd3fcef98042912f1c5101daad01802d
denisfl <delchyve@gmail.com>
2016-07-21 10:51:27 +0300

    change Notes layout

--- src/states/my/notes/NotesView.js
+++ src/states/my/notes/NotesView.js
@@ -1,47 +1,54 @@
-import React from "react";
-import {connect} from "react-redux";
-import {Link} from "react-router";
-import {map} from "ramda";
-import {getNodesList} from "nodes/redux/actions";
-import LoadMore from "commons/pagination/components/LoadMore";
-import {Card, CardItem, CardDivider, CardRows, CardRow} from "commons/cards/components";
-import moment from "moment";
-import {LinearProgress, RaisedButton, FlatButton, TextField} from "material-ui";
-import NoteForm from "nodes/components/NoteForm";
+import './styles.css';
+import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router';
+import { map } from 'ramda';
+import { getNodesList } from 'nodes/redux/actions';
+import LoadMore from 'commons/pagination/components/LoadMore';
+import { Card, CardItem } from 'commons/cards/components';
+import moment from 'moment';
+import NoteForm from 'nodes/components/NoteForm';
 
 const mapStateToProps = (state) => ({
   nodes: state.nodes.list,
   nodeId: state.resolve.params.nodeId,
-  q: state.resolve.query.q || ""
-})
+  q: state.resolve.query.q || '',
+});
 
 const mapDispatchToProps = {
-  setPage: (p)=>
-    getNodesList({...p, layer: "Draft"})
-}
+  setPage: (p) =>
+    getNodesList({ ...p, layer: 'Draft' }),
+};
 
-const NotesView = ({nodes, setPage, nodeId, q}) => {
+const NotesView = ({ nodes, setPage, nodeId, q }) => {
 
-  const haveMore = nodes.values.length < nodes.total
+  const haveMore = nodes.values.length < nodes.total;
 
-  const loadMore = () => setPage({q, append: true, limit: nodes.limit, offset: nodes.offset + nodes.limit})
+  const loadMore = () =>
+    setPage({ q, append: true, limit: nodes.limit, offset: nodes.offset + nodes.limit });
 
   return (
-    <div className="Content">
+    <div className="Notes">
+      <div className="Notes-container">
+        <div className="Notes-list">
+          {map((n) =>
+            <Card key={n.id} isActive={n.id === nodeId}>
+              <CardItem>
+                <Link to={"/my/notes/" + n.id}>
+                  {n.title || '***'}, {moment(n.lastUpdated).fromNow()}
+                </Link>
+              </CardItem>
+            </Card>, nodes.values)
+          }
 
-      {!q && <NoteForm/>}
-
-      { map((n) =>
-        <Card key={n.id} isActive={n.id === nodeId}>
-          <CardItem>
-            <Link to={"/my/notes/"+n.id}>{n.title || "***"}, {moment(n.lastUpdated).fromNow()}</Link>
-          </CardItem>
-        </Card>, nodes.values) }
-
-      <LoadMore action={loadMore} haveMore={haveMore}/>
-
+          <LoadMore action={loadMore} haveMore={haveMore} />
+        </div>
+        <div className="Notes-content">
+          {!q && <NoteForm />}
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(NotesView)
+export default connect(mapStateToProps, mapDispatchToProps)(NotesView);
