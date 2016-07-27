ec9436cd3f2982a8a04def54cd97b267980acbd1
Dmitry Kurinskiy <name.alari@gmail.com>
2016-07-27 12:04:14 +0300

    show controls in react-player

--- src/nodes/components/Markdown.js
+++ src/nodes/components/Markdown.js
@@ -1,28 +1,28 @@
-import React from "react";
-import ReactMarkdown from "react-markdown";
-import {equals, length} from "ramda";
-import ReactPlayer from "react-player";
+import React from 'react';
+import ReactMarkdown from 'react-markdown';
+import { equals, length } from 'ramda';
+import ReactPlayer from 'react-player';
 
 const renderers = {
   Paragraph: React.createClass({
-    shouldComponentUpdate: function (nextProps, nextState) {
+    shouldComponentUpdate(nextProps, nextState) {
       return !equals(nextProps, this.props);
     },
 
-    render: function () {
+    render() {
       const children = this.props.children
 
-      if (length(children) === 1 && typeof children[0] === "string" && ReactPlayer.canPlay(children[0])) {
-        return (<ReactPlayer url={children[0]}/>)
+      if (length(children) === 1 && typeof children[0] === 'string' && ReactPlayer.canPlay(children[0])) {
+        return (<ReactPlayer url={children[0]} controls={true} />);
       } else {
-        return (<p>{children}</p>)
+        return (<p>{children}</p>);
       }
-    }
-  })
+    },
+  }),
 }
 
-export default ({content}) => (!!content ? <ReactMarkdown
+export default ({ content }) => (!!content ? <ReactMarkdown
   softBreak="br"
   renderers={renderers}
   source={content}
-/> : null)
+/> : null);
