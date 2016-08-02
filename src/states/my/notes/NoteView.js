f77c559527cc1af6a0e45ab522c936e56922856b
denisfl <delchyve@gmail.com>
2016-08-02 12:46:54 +0300

    update link 'pinnedToNodes' to the header

--- src/states/my/notes/NoteView.js
+++ src/states/my/notes/NoteView.js
@@ -14,27 +14,22 @@ const mapStateToProps = (state) => ({
 const NoteView = ({ state: { isEditing = false }, setState, node }) =>
   <div>
     <div className="Notes-controls">
+      {node.pinnedToNodes &&
+        <div>
+          {map(p =>
+            <Link key={p.id} to={`/my/node/${p.id}`} >
+              {p.title}
+            </Link>, node.pinnedToNodes)
+          }
+        </div>
+      }
+      <div className="Notes-controlsDivider" />
       <Button
         onClick={() => setState({ isEditing: !isEditing })}
         title={isEditing ? 'В просмотр' : 'Редактировать'}
       />
     </div>
     {isEditing ? <NodeForm /> : <NodeText node={node} />}
-
-    {node.pinnedToNodes &&
-      <div>
-        <hr />
-        {map(p =>
-          <Link
-            key={p.id}
-            to={`/my/node/${p.id}`}
-            style={{ fontSize: 'small', color: 'gray', padding: '1rem' }}
-          >
-            {p.title}
-          </Link>, node.pinnedToNodes)
-        }
-      </div>
-    }
   </div>;
 
 export default connect(mapStateToProps)(decorateWithState(NoteView));
