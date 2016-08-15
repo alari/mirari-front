14fa0ee2ec4d6fb901288b5050c0d37140380d92
Dmitry Kurinskiy <name.alari@gmail.com>
2016-08-15 11:54:43 +0300

    yandex metrika

--- src/store/sagas.js
+++ src/store/sagas.js
@@ -4,12 +4,14 @@ import nodeCreatedSaga from 'nodes/sagas/nodeCreated';
 import nodeEnterSaga from 'nodes/sagas/nodeEnter';
 import nodeSseSaga from 'nodes/sagas/nodeSse';
 import nodeMovedToDraft from 'nodes/sagas/nodeMovedToDraft';
+import trackSaga from 'commons/resolve/sagas/trackSaga';
 
-export default  [
+export default [
+  trackSaga('UA-69497453-1', '39055300'),
   cookieSaga,
   resolveStateSaga,
   nodeSseSaga,
   nodeEnterSaga,
   nodeCreatedSaga,
-  nodeMovedToDraft
-]
+  nodeMovedToDraft,
+];
