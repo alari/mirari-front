93bfdb152f446420f80b1084df4435265e5dbe8b
denisfl <delchyve@gmail.com>
2016-11-13 20:10:22 +0300

    💩-->🍭  👻

--- src/states/experimental/ExperimentalView.js
+++ src/states/experimental/ExperimentalView.js
@@ -30,42 +30,49 @@ const mapStateToProps = (state) => ({
   user: state.auth.user
 })
 
-export default connect(mapStateToProps)(({title, link, meta, user, children}) => <div>
-  <Helmet
-    title={title}
-    defaultTitle="Мирари"
-    link={link}
-    meta={meta}
-  />
+export default connect(mapStateToProps)(({title, link, meta, user, children}) =>
+  <div>
+    <Helmet
+      title={title}
+      defaultTitle="Мирари"
+      link={link}
+      meta={meta}
+    />
 
-  <Headroom tolerance={5} offset={200} classes={{
-    initial: 'headroom',
-    pinned: 'is-scrollUp',
-    unpinned: 'is-scrollDown'
-  }}
-  >
-    <div className="ExpHeader">
-      <div className="ExpHeader-item">
-        <a className="ExpHeader-itemLink" href="#">К контексту</a>
+    <Headroom tolerance={5} offset={200} classes={{
+      initial: 'headroom',
+      pinned: 'is-scrollUp',
+      unpinned: 'is-scrollDown'
+    }}
+    >
+      <div className="ExpHeader">
+        <div className="ExpHeader-natural">
+          <div className="ExpHeader-container">
+            <div className="ExpHeader-item">
+              <a className="ExpHeader-itemLink" href="#">К контексту</a>
+            </div>
+            <div className="ExpHeader-item--divider"/>
+            <div className="ExpHeader-item ExpHeader-logo">
+              <Link to="/" className="ExpHeader-itemLink ExpHeader-logoLink">
+                <svg className="ExpHeader-logoIcon" viewBox="0 0 144 144">
+                  <path d="M99.0000011,5.23365386 C90.6621179,1.85856437 81.5481044,0 72,0 C62.4518956,0 53.3378821,1.85856437 44.9999989,5.23365386 L45,24.0722737 C52.9757653,19.5694753 62.1878161,17 72,17 C81.8121839,17 91.0242347,19.5694753 99,24.0722737 L99,5.23364649 Z M123,21.176776 C135.97753,34.1991633 144,52.1627075 144,72 C144,91.8372925 135.97753,109.800837 123,122.823224 L123,92.6301138 C125.579503,86.2595105 127,79.2956426 127,72 C127,64.7043574 125.579503,57.7404895 123,51.3698862 L123,21.176776 Z M99.0000011,138.766346 C90.6621179,142.141436 81.5481044,144 72,144 C62.4518956,144 53.3378821,142.141436 44.9999989,138.766346 L45,119.927726 C52.9757653,124.430525 62.1878161,127 72,127 C81.8121839,127 91.0242347,124.430525 99,119.927726 L99,138.766354 Z M21,21.176776 C8.02246962,34.1991633 0,52.1627075 0,72 C0,91.8372925 8.02246962,109.800837 21,122.823224 L21,92.6301138 C18.4204966,86.2595105 17,79.2956426 17,72 C17,64.7043574 18.4204966,57.7404895 21,51.3698862 L21,21.176776 Z M57,56 L89,56 L89,88 L57,88 L57,56 Z" fill="#000000" />
+                </svg>
+              </Link>
+            </div>
+            <div className="ExpHeader-item--divider"/>
+            <div className="ExpHeader-item">
+              <a className="ExpHeader-itemLink" href="#">Стать участником | Вход</a>
+            </div>
+          </div>
+        </div>
+        <div className="ExpHeader-substitute" />
       </div>
-      <div className="ExpHeader-item--divider"/>
-      <div className="ExpHeader-item ExpHeader-logo">
-        <Link to="/" className="ExpHeader-itemLink ExpHeader-logoLink">
-          <svg className="ExpHeader-logoIcon" viewBox="0 0 144 144">
-            <path d="M99.0000011,5.23365386 C90.6621179,1.85856437 81.5481044,0 72,0 C62.4518956,0 53.3378821,1.85856437 44.9999989,5.23365386 L45,24.0722737 C52.9757653,19.5694753 62.1878161,17 72,17 C81.8121839,17 91.0242347,19.5694753 99,24.0722737 L99,5.23364649 Z M123,21.176776 C135.97753,34.1991633 144,52.1627075 144,72 C144,91.8372925 135.97753,109.800837 123,122.823224 L123,92.6301138 C125.579503,86.2595105 127,79.2956426 127,72 C127,64.7043574 125.579503,57.7404895 123,51.3698862 L123,21.176776 Z M99.0000011,138.766346 C90.6621179,142.141436 81.5481044,144 72,144 C62.4518956,144 53.3378821,142.141436 44.9999989,138.766346 L45,119.927726 C52.9757653,124.430525 62.1878161,127 72,127 C81.8121839,127 91.0242347,124.430525 99,119.927726 L99,138.766354 Z M21,21.176776 C8.02246962,34.1991633 0,52.1627075 0,72 C0,91.8372925 8.02246962,109.800837 21,122.823224 L21,92.6301138 C18.4204966,86.2595105 17,79.2956426 17,72 C17,64.7043574 18.4204966,57.7404895 21,51.3698862 L21,21.176776 Z M57,56 L89,56 L89,88 L57,88 L57,56 Z" fill="#000000" />
-          </svg>
-        </Link>
-      </div>
-      <div className="ExpHeader-item--divider"/>
-      <div className="ExpHeader-item">
-        <a className="ExpHeader-itemLink" href="#">Стать участником | Вход</a>
-      </div>
-    </div>
-  </Headroom>
+    </Headroom>
 
-  {children || <ExperimentalContent />}
+    {children || <ExperimentalContent />}
 
-</div>);
+  </div>
+);
 
 const ExperimentalContent = () =>
   <div className="ExpApp">
@@ -87,25 +94,23 @@ const ExperimentalContent = () =>
         </div>
       </article>
     </div>
-    <div className="cta-block">
-      <div style={{textAlign:'center'}}>Давайте взаимодействовать</div>
-      <table>
-        <tbody>
-        <tr>
-          <td width="33%">
+    <div className="Interaction">
+      <div className="Interaction-container">
+        <div className="Interaction-title">Давайте взаимодействовать</div>
+        <div className="Interaction-list">
+          <div className="Interaction-item">
             <button>Обсудить</button><br/>
             <small>Будьте первым!</small>
-          </td>
-          <td width="34%">
+          </div>
+          <div className="Interaction-item">
             <button>Подписаться</button> <br/>
             <small>На новые работы автора, через Telegram или email</small>
-          </td>
-          <td width="33%">
+          </div>
+          <div className="Interaction-item">
             <button>Стать участником</button><br/>
             <small>Как автор, читатель, держатель инициативы</small>
-          </td>
-        </tr>
-        </tbody>
-      </table>
+          </div>
+        </div>
+      </div>      
     </div>
   </div>
